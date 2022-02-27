d0d894520b7127709e227a2e1f5eed81d09b4e31
JCH95 <jhorger95@gmail.com>
2022-02-26 22:23:59 -0600

    added gitignore file, added structure for writeFile, added beginning questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,73 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptUser = () => {   
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project? (Required)',
+            name: 'title',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'Please provide a description of your project',
+            name: 'description',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+           type: 'input',
+           message: 'What are the steps needed to install this project? (Required)',
+           name: 'installation',
+           validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'Provide instructions and examples on how it works. Include screenshots if needed.'
+        }
+        
+    ]);
+};
+// Create a function to write README file
+const writeFile = fileContent => {
+    return new Promise((resolve, rejectt) => {
+        fs.writeFile('./dist/ReadME.md', fileContent, err => {
+            // If Error, reject Promise and sent error info to catch method
+            if (err) {
+                rejectt(err);
+                return
+            }
+            // Resolve if everything worked and send data to then method
+            resolve({
+                ok: true,
+                message: 'ReadME file created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
 function init() {}
